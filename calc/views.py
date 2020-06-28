118eac7485c0e9983fd76403a35531daefd37833
Sarath3395 <Sarath3395@users.noreply.github.com>
2020-06-28 05:39:35 +0100

    basicpage

--- calc/views.py
+++ calc/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello world")
+    return render(request, 'home.html', {'name':'sarath'})
 
